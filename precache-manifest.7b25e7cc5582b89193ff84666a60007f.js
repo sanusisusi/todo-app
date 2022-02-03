d6d4d7424d8975b440ce01416f5a36a7c60e5582
Sanusi Abdulkadir <sanusiabdulkadir00@gmail.com>
2022-02-04 00:10:52 +0100

    Updates

---
+++ precache-manifest.7b25e7cc5582b89193ff84666a60007f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef09bb2a217e42b45f71ff6acf14a7b4",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "28f4f8c71a45d48e6b9d",
+    "url": "/todo-app/static/css/main.2fc5a4a4.chunk.css"
+  },
+  {
+    "revision": "c2043bf5f2e098cb370f",
+    "url": "/todo-app/static/js/2.1f01020a.chunk.js"
+  },
+  {
+    "revision": "28f4f8c71a45d48e6b9d",
+    "url": "/todo-app/static/js/main.1c5ff187.chunk.js"
+  },
+  {
+    "revision": "5946997cbbde999498f6",
+    "url": "/todo-app/static/js/runtime-main.4784d01e.js"
+  }
+]);
